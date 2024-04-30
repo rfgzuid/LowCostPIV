86d0ef1669a42d042ac847d2fa32fe2397afead5
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-04-30 19:03:48 +0200

    Made an automatic frame processer for saving jpgs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
+import lib
 
-if __name__ == "__main__":
+
+def process_video(fps: tuple[float, float], folder=None) -> None:
     cap = cv2.VideoCapture('IMG_4255.MOV')
     frames = []
 
@@ -15,15 +17,35 @@ if __name__ == "__main__":
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frames.append(gray)
 
-    reference = frames[0]
-    for idx, frame in enumerate(frames):
-        frames[idx] = np.where(np.abs(frame - reference) > 10, 255, 0).astype(np.uint8)
+    new_frames = []
+    new_frames.append(np.zeros_like(frames[0]))
 
-    for frame in frames:
-        cv2.imshow('frame', frame)
+    # Pre-processing: black out background
+    for idx, frame in enumerate(frames[1:]):
+        reference = frames[idx - 1]
+        difference = cv2.absdiff(frame, reference)
+        _, mask = cv2.threshold(difference, 50, 255, cv2.THRESH_BINARY)
+        new_frames.append(mask)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # If a folder is specified, save the processed frames. Else show a video of the processed frames
+    for idx, frame in enumerate(new_frames):
+        if folder is not None:
+            cv2.imwrite(f'folder/{idx}.jpg', frame)
+        else:
+            img = cv2.putText(frame, f't = {(idx / fps[0]):.3f} s', (20, 50),
+                              cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow('PROCESSED VIDEO', img)
+
+            if cv2.waitKey(round(1000 / fps[1])) & 0xFF == ord('q'):
+                break
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    capture_rate = 240  # Hz
+    playback_rate = 30  # Hz - Iphone standard
+    fps_settings = (capture_rate, playback_rate)
+
+    process_video(fps_settings, folder='video frames')
