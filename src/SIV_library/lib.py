f554f105612d6e60a236506e68d3e6e0794dccd2
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-05-28 11:14:49 +0200

    Bug fixing

--- src/SIV_library/lib.py
+++ src/SIV_library/lib.py
@@ -1,4 +1,5 @@
-from torch.nn.functional import conv2d, pad
+from torch.nn.functional import conv2d, pad, grid_sample
+from torchvision.transforms import Resize, InterpolationMode
 import torch
 import numpy as np
 
@@ -34,25 +35,51 @@ def block_match(windows: torch.Tensor, areas: torch.Tensor, mode: int) -> torch.
     return res / (window_rows * window_cols)
 
 
-def moving_window_array(array: torch.Tensor, window_size, overlap,
-                           area: tuple[int, int, int, int] | None = None) -> torch.Tensor:
-    if area is None:
-        area = (0, 0, 0, 0)
-    left, right, top, bottom = area
-    padded = pad(array, area)
-    shape = padded.shape
-
+def window_array(array: torch.Tensor, window_size, overlap) -> torch.Tensor:
+    shape = array.shape
     strides = (shape[-1] * (window_size - overlap), (window_size - overlap), shape[-1], 1)
 
-    shape = (
-        int((shape[-2] - window_size - top - bottom) / (window_size - overlap)) + 1,
-        int((shape[-1] - window_size - left - right) / (window_size - overlap)) + 1,
-        window_size + top + bottom,
-        window_size + left + right,
-    )
+    shape = (int((shape[-2] - window_size) / (window_size - overlap)) + 1,
+             int((shape[-1] - window_size) / (window_size - overlap)) + 1,
+             window_size, window_size)
 
-    return (torch.as_strided(padded, size=shape, stride=strides)
-            .reshape(-1, window_size + top + bottom, window_size + left + right))
+    return (torch.as_strided(array, size=shape, stride=strides)
+            .reshape(-1, window_size, window_size))
+
+
+def search_array(array: torch.Tensor, window_size, overlap,
+                 area: tuple[int, int, int, int] | None = None,
+                 offsets: torch.Tensor | None = None) -> torch.Tensor:
+    iters = get_field_shape(array.shape, window_size, overlap)
+
+    dx, dy = torch.round(offsets[0]).int(), torch.round(offsets[1]).int()
+    dx_max, dy_max = torch.max(torch.abs(dx)).item(), torch.max(torch.abs(dy)).item()
+
+    left, right, top, bottom = area
+    padding = (left + dx_max, right + dx_max, top + dy_max, bottom + dy_max)
+    array = pad(array, padding)
+
+    areas = torch.zeros((iters[0]*iters[1], window_size+top+bottom, window_size+left+right), dtype=torch.uint8)
+
+    for j in range(iters[0]):
+        for i in range(iters[1]):
+            offset_x, offset_y = int(offsets[0, i, j]), int(offsets[1, i, j])
+            area = array[j*overlap+offset_y+dy_max:j*overlap+window_size+top+bottom+offset_y+dy_max,
+                         i*overlap+offset_x+dx_max:i*overlap+window_size+left+right+offset_x+dx_max]
+            areas[i+j*iters[1], :, :] = area
+
+            if i + j*iters[1] == 3550:
+                start = (i * overlap + offset_x + dx_max,
+                         j*overlap+offset_y+dy_max)
+                end = (i*overlap+window_size+left+right+offset_x+dx_max,
+                       j*overlap+window_size+top+bottom+offset_y+dy_max)
+
+                img = cv2.rectangle(array.numpy(), start, end, (255, 0, 0), 1)
+                img = cv2.resize(img, (500, 500), cv2.INTER_AREA)
+                cv2.imshow('img', img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+    return areas
 
 
 def correlation_to_displacement(corr: torch.Tensor, n_rows, n_cols, mode: int = 0):
@@ -88,16 +115,7 @@ def correlation_to_displacement(corr: torch.Tensor, n_rows, n_cols, mode: int = 
             edge_cases[idx] = True
             continue
 
-        if mode == 0:
-            x, y = np.meshgrid(np.arange(-round(cols // 2), round(cols // 2) + 1, 1),
-                               np.arange(-round(rows // 2), round(rows // 2) + 1, 1))
-            fig, ax = plt.subplots()
-            ax = fig.add_subplot(projection='3d')
-            ax.plot_surface(x, y, field.numpy(), color='b', alpha=0.5, label='correlation')
-            ax.scatter(col_idx-cols/2, row_idx-rows/2, peak_val, color='r')
-            plt.show()
-
-        neighbors[idx] = torch.tensor(field[row_idx-1:row_idx+2, col_idx-1:col_idx+2])
+        neighbors[idx] = field.clone()[row_idx-1:row_idx+2, col_idx-1:col_idx+2]
 
     # Gaussian interpolation for correlation
     if mode == 0:
@@ -139,11 +157,44 @@ def correlation_to_displacement(corr: torch.Tensor, n_rows, n_cols, mode: int = 
         min_locs[~singular] = torch.linalg.solve(a[~singular], b[~singular])
 
         s_x, s_y = min_locs[:, 0], min_locs[:, 1]
+
+        # cases where interpolation fails: override with s = 0.
+        s_x = torch.where(torch.abs(s_x) >= 1., 0., s_x)
+        s_y = torch.where(torch.abs(s_y) >= 1., 0., s_y)
         s_x[edge_cases], s_y[edge_cases] = 0., 0.
+
+        ############################################################################
+
+        # idx = 3550
+        #
+        # xs, ys = np.linspace(-1, 1, 51), np.linspace(-1, 1, 51)
+        # a0, a1, a2, a3, a4, a5 = a0[idx], a1[idx], a2[idx], a3[idx], a4[idx], a5[idx]
+        # min_loc = min_locs[idx]
+        #
+        # def surface(xi, yi):
+        #     return a0 + a1 * xi + a2 * yi + a3 * xi * yi + a4 * xi ** 2 + a5 * yi ** 2
+        #
+        # heightmap = surface(xs[None, :], ys[:, None])
+        # xs, ys = np.meshgrid(xs, ys)
+        #
+        # min_height = surface(*min_loc)
+        # print('u', min_loc, min_height)
+        #
+        # x, y = np.meshgrid(np.arange(-1, 2, 1), np.arange(-1, 2, 1))
+        #
+        # fig, ax = plt.subplots()
+        # ax = fig.add_subplot(projection='3d')
+        #
+        # ax.plot_surface(x, -y, neighbors[idx], color='r', alpha=0.5, label='SAD')
+        # ax.plot_surface(xs, ys, heightmap.reshape(51, 51), color='b', alpha=0.5, label='interp')
+        # ax.scatter(*min_loc, min_height, color='g')
+        #
+        # ax.set_xlabel('x')
+        # ax.set_ylabel('y')
+        # plt.show()
 
     m2d = torch.cat((m // rows, m % cols), -1)
 
-    s_x[:], s_y[:] = 0., 0.
     u = m2d[:, 1][:, None] + s_x[:, None]
     v = m2d[:, 0][:, None] + s_y[:, None]
 
@@ -156,8 +207,8 @@ def correlation_to_displacement(corr: torch.Tensor, n_rows, n_cols, mode: int = 
     torch.nan_to_num_(v)
     torch.nan_to_num_(u)
 
-    u = u.reshape(n_rows, n_cols).cpu().numpy()
-    v = v.reshape(n_rows, n_cols).cpu().numpy()
+    u = u.reshape(n_rows, n_cols)
+    v = v.reshape(n_rows, n_cols)
 
     return u, v
 
@@ -177,27 +228,111 @@ def get_x_y(image_size, search_area_size, overlap):
 
 
 class SIVDataset(Dataset):
-    def __init__(self, folder: str, ROI: tuple[int, int, int, int] | None):
+    def __init__(self, folder: str):
         # assume the files are sorted and all have the correct file type
         filenames = [os.path.join(folder, name) for name in os.listdir(folder)]
         self.img_pairs = list(zip(filenames[:-1], filenames[1:]))
-
-        self.ROI = ROI  # slice images on coords (top, bottom, left, right)
 
     def __len__(self):
         return len(self.img_pairs)
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
         pair = self.img_pairs[index]
+        img_a, img_b = cv2.imread(pair[0], cv2.IMREAD_GRAYSCALE), cv2.imread(pair[1], cv2.IMREAD_GRAYSCALE)
+        return torch.tensor(img_a, dtype=torch.uint8), torch.tensor(img_b, dtype=torch.uint8)
 
-        img_b = cv2.imread(pair[1], cv2.IMREAD_GRAYSCALE)
-        img_a = cv2.imread(pair[0], cv2.IMREAD_GRAYSCALE)
 
-        if self.ROI is not None:
-            img_a = img_a[self.ROI[0]:self.ROI[1], self.ROI[2]:self.ROI[3]]
-            img_b = img_b[self.ROI[0]:self.ROI[1], self.ROI[2]:self.ROI[3]]
+class SIV:
+    def __init__(self, folder: str, device: torch.device="cpu",
+                 window_size: int=128, overlap: int=64, search_area: tuple[int, int, int, int]=(0, 0, 0, 0),
+                 multipass: int=1, multipass_scale: float=2.,
+                 dt: float=1/240) -> None:
+        self.dataset = SIVDataset(folder=folder)
+        self.device = device
+        self.window_size, self.overlap, self.search_area = window_size, overlap, search_area
+        self.multipass, self.multipass_scale = multipass, multipass_scale
+        self.dt = dt
 
-        img_a = torch.tensor(img_a, dtype=torch.uint8)
-        img_b = torch.tensor(img_b, dtype=torch.uint8)
+    def run(self, mode: int):
+        for idx, data in enumerate(self.dataset):
+            img_a, img_b = data
+            img_a, img_b = img_a.to(self.device), img_b.to(self.device)
 
-        return img_a, img_b
+            n_rows, n_cols = get_field_shape(img_a.shape, self.window_size, self.overlap)
+
+            u, v = torch.zeros((n_rows, n_cols)), torch.zeros((n_rows, n_cols))
+
+            for k in range(self.multipass):
+                scale = self.multipass_scale ** (k - self.multipass + 1)
+                window_size, overlap = int(self.window_size * scale), int(self.overlap * scale)
+
+                new_size = (round(img_a.shape[1] * scale), round(img_a.shape[0] * scale))
+                resize = Resize(new_size, InterpolationMode.BILINEAR)
+                a, b = resize(img_a[None, :, :]).squeeze(), resize(img_b[None, :, :]).squeeze()
+
+                offset = torch.stack((u, v))
+                print(offset[:, 3550//n_rows, 3550%n_cols])
+
+                x, y = get_x_y(a.shape, window_size, overlap)
+                x, y = x.reshape(n_rows, n_cols), y.reshape(n_rows, n_cols)
+
+                window = window_array(a, window_size, overlap)
+                area = search_array(b, window_size, overlap, area=self.search_area, offsets=offset)
+
+                match = block_match(window, area, mode)
+                du, dv = correlation_to_displacement(match, n_rows, n_cols, mode)
+
+                print(du[3550//n_rows, 3550%n_cols], dv[3550//n_rows, 3550%n_cols])
+
+                u, v = u + du*self.multipass_scale, v + dv*self.multipass_scale
+
+                fig, ax = plt.subplots(1, 1)
+
+                frame = a.cpu().numpy()
+                ax.imshow(frame, cmap='gray')
+                ax.quiver(x, y, u, -v, color='red', scale=1, scale_units='xy')
+
+                ax.set_title('Correlation' if mode == 0 else 'SAD')
+                ax.set_axis_off()
+
+                fig.tight_layout()
+                plt.show()
+        return x, y, u, -v
+
+
+class OpticalFlow:
+    def __init__(self, folder: str=None, device: torch.device="cpu",
+                 multipass: int=1, multipass_scale: float=2., dt: float=1/240) -> None:
+        self.dataset = SIVDataset(folder=folder)
+        self.device = device
+        self.multipass, self.multipass_scale = multipass, multipass_scale
+        self.dt = dt
+
+    def run(self, mode: int):
+        for idx, data in enumerate(self.dataset):
+            if idx == 0:
+                img_a, img_b = data
+                n_rows, n_cols = get_field_shape(img_a.shape, self.window_size, self.overlap)
+                u, v = torch.zeros((n_rows, n_cols)), torch.zeros((n_rows, n_cols))
+
+                for k in range(self.multipass):
+                    scale = self.multipass_scale ** (k - self.multipass + 1)
+                    new_size = (round(img_a.shape[1] * scale), round(img_a.shape[0] * scale))
+
+                    a, b = cv2.resize(img_a, new_size, cv2.INTER_AREA), cv2.resize(img_b, new_size, cv2.INTER_AREA)
+                    a = torch.tensor(a, dtype=torch.uint8).to(self.device)
+                    b = torch.tensor(b, dtype=torch.uint8).to(self.device)
+
+                    x, y = get_x_y(a.shape, window_size, overlap)
+                    x, y = x.reshape(n_rows, n_cols), y.reshape(n_rows, n_cols)
+
+                    grid = torch.zeros(1, *a.shape, 2)
+
+                    # https://discuss.pytorch.org/t/cv2-remap-in-pytorch/99354/10
+                    du_interp = grid_sample(x, y, du, mode='bilinear')
+
+                    # du_interp = interpolate(du[None, None, :, :], a.shape, mode='bilinear').squeeze()
+                    # dv_interp = interpolate(dv[None, None, :, :], a.shape, mode='bilinear').squeeze()
+
+                    u, v = u + du, v + dv
+        return x, y, u, v
