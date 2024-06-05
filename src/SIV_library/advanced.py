fdbce8586d0165bbe1a6b0f25ed0836ba8f076b8
Ruben Zuidgeest <r.f.g.zuidgeest@student.tudelft.nl>
2024-06-05 17:04:01 +0200

    match refine implemented

--- src/SIV_library/advanced.py
+++ src/SIV_library/advanced.py
@@ -11,7 +11,7 @@ class Warp(torch.nn.Module):
 
     def __init__(self, x, y, u, v):
         super().__init__()
-        self.x, self.y, self.u, self.v = x, y, u.cpu(), v.cpu()
+        self.x, self.y, self.u, self.v = x, y, u, v
         self.idx = 0
 
         self.apply_to = ['a']  # apply this transform to ONLY the first image of the pair
@@ -24,7 +24,7 @@ class Warp(torch.nn.Module):
 
         # https://discuss.pytorch.org/t/image-warping-for-backward-flow-using-forward-flow-matrix-optical-flow/99298
         # https://discuss.pytorch.org/t/solved-torch-grid-sample/51662/2
-        grid = torch.stack((x[self.idx], y[self.idx]), dim=-1)
+        grid = torch.stack((x[self.idx], y[self.idx]), dim=-1).to(x.device)
         v_grid = grid + torch.stack((-self.u[self.idx] / (cols / 2), self.v[self.idx] / (rows / 2)), dim=-1)
 
         img_new = grid_sample(image.float(), v_grid[None, :, :, :], mode='bicubic').to(torch.uint8)
@@ -39,10 +39,8 @@ class Warp(torch.nn.Module):
         self.v = interpolate(self.v[None, :, :, :], img_shape, mode='bicubic').squeeze()
 
         y, x = torch.meshgrid(torch.arange(0, img_shape[0], 1), torch.arange(0, img_shape[1], 1))
-        self.x, self.y = x.expand(self.x.shape[0], -1, -1), y.expand(self.y.shape[0], -1, -1)
-
-
-# ----------------------------------------------------------------------------------------------------------------------
+        x, y = x.expand(self.x.shape[0], -1, -1), y.expand(self.y.shape[0], -1, -1)
+        self.x, self.y = x.to(self.x.device), y.to(self.y.device)
 
 
 def coarse_to_fine(optical: OpticalFlow, num_passes: int = 3, scale_factor: float = 1/2):
@@ -59,7 +57,8 @@ def coarse_to_fine(optical: OpticalFlow, num_passes: int = 3, scale_factor: floa
 
     for idx, size in enumerate(sizes):
         y, x = torch.meshgrid(torch.arange(0, size[0], 1), torch.arange(0, size[1], 1))
-        x, y = x.expand(len(optical.dataset), -1, -1), y.expand(len(optical.dataset), -1, -1)
+        x = x.expand(len(optical.dataset), -1, -1).to(optical.device)
+        y = y.expand(len(optical.dataset), -1, -1).to(optical.device)
 
         resize = Resize(size, InterpolationMode.BICUBIC)
         resize.apply_to = ['a', 'b']  # apply the resize transform to both images in the pair
@@ -83,12 +82,17 @@ def match_refine(matching: SIV, optical: OpticalFlow, mode: int = 1):
     runs the matching algorithm and refines the result with optical flow
     https://link-springer-com.tudelft.idm.oclc.org/article/10.1007/s00348-019-2820-4?fromPaywallRec=false
     """
+    img_shape = matching.dataset.img_shape
     x, y, u, v = matching.run(mode=mode)
 
     warp = Warp(x, y, u, v)
+
+    warp.interpolate_field(img_shape)
+    x, y, u, v = warp.x, warp.y, warp.u, warp.v
+
     optical.dataset.transforms = [warp]
 
-    x, y, du, dv = optical.run()
+    _, _, du, dv = optical.run()
     u, v = u + du, v + dv
 
     return x, y, u, -v
--- src/SIV_library/lib.py
+++ src/SIV_library/lib.py
@@ -11,13 +11,14 @@ from tqdm import tqdm
 
 
 class SIVDataset(Dataset):
-    def __init__(self, folder: str, transforms: list | None = None) -> None:
+    def __init__(self, folder: str, transforms: list | None = None, device: str = "cpu") -> None:
         # assume the files are sorted and all have the correct file type
         filenames = [os.path.join(folder, name) for name in os.listdir(folder)]
         self.img_pairs = list(zip(filenames[:-1], filenames[1:]))
 
-        self.img_shape = cv2.imread(filenames[0]).shape
+        self.img_shape = cv2.imread(filenames[0], cv2.IMREAD_GRAYSCALE).shape
         self.transforms = transforms
+        self.device = device
 
     def __len__(self) -> int:
         return len(self.img_pairs)
@@ -26,7 +27,8 @@ class SIVDataset(Dataset):
         pair = self.img_pairs[index]
         img_a, img_b = cv2.imread(pair[0], cv2.IMREAD_GRAYSCALE), cv2.imread(pair[1], cv2.IMREAD_GRAYSCALE)
 
-        img_a, img_b = torch.tensor(img_a, dtype=torch.uint8), torch.tensor(img_b, dtype=torch.uint8)
+        img_a = torch.tensor(img_a, dtype=torch.uint8, device=self.device)
+        img_b = torch.tensor(img_b, dtype=torch.uint8, device=self.device)
         img_a, img_b = img_a[None, None, :, :], img_b[None, None, :, :]  # batch and channel dimension for transforms
 
         if self.transforms is not None:
@@ -45,7 +47,7 @@ class SIV:
                  search_area: tuple[int, int, int, int] = (0, 0, 0, 0),
                  ) -> None:
 
-        self.dataset = SIVDataset(folder=folder)
+        self.dataset = SIVDataset(folder=folder, device=device)
         self.device = device
         self.window_size, self.overlap, self.search_area = window_size, overlap, search_area
 
@@ -63,7 +65,7 @@ class SIV:
                 n_rows, n_cols = get_field_shape(img_shape, self.window_size, self.overlap)
 
                 x, y = get_x_y(self.img_shape, self.window_size, self.overlap)
-                x, y = x.reshape(n_rows, n_cols), y.reshape(n_rows, n_cols)
+                x, y = x.reshape(n_rows, n_cols).to(self.device), y.reshape(n_rows, n_cols).to(self.device)
                 x, y = x.expand(len(self.dataset), -1, -1), y.expand(len(self.dataset), -1, -1)
 
                 u, v = (torch.zeros((len(self.dataset), n_rows, n_cols), device=self.device),
@@ -89,7 +91,7 @@ class OpticalFlow:
                  ) -> None:
 
         self.folder = folder
-        self.dataset = SIVDataset(folder=folder)
+        self.dataset = SIVDataset(folder=folder, device=device)
         self.device = device
         self.alpha, self.num_iter, self.eps = alpha, num_iter, eps
 
@@ -103,7 +105,8 @@ class OpticalFlow:
                 rows, cols = a.shape[-2:]
 
                 y, x = torch.meshgrid(torch.arange(0, rows, 1), torch.arange(0, cols, 1))
-                x, y = x.expand(len(self.dataset), -1, -1), y.expand(len(self.dataset), -1, -1)
+                x = x.expand(len(self.dataset), -1, -1).to(self.device)
+                y = y.expand(len(self.dataset), -1, -1).to(self.device)
 
                 u, v = (torch.zeros((len(self.dataset), rows, cols), device=self.device),
                         torch.zeros((len(self.dataset), rows, cols), device=self.device))
